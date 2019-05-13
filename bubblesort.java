68dfcafe72649c9471da796f12a1cc6a427318a4
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-13 19:36:23 +0000

    Added recursive bubble sort from left to right

--- bubblesort.java
+++ bubblesort.java
@@ -1,38 +1,40 @@
 package com.company;
 
-public class bubblesort {
+public class bubblesort{
 
     public static void main(String[] args) {
 
-        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+        int intArray[] = {20, 35, -15, 7, 55, 1, -22};
+        bubbleSort(intArray, intArray.length);
 
-        for (int lastUnsortedIndex =  intArray.length - 1; lastIndex > 0; lastUnsortedIndex--) {
-            for (int i = 0; i < lastUnsortedIndex; i++) {
-                if (intArray[i] > intArray[i+1]) {
-                    swap(intArray, i,i+1);
-                }
-            }
-        }
+        // Iterative bubblesort from left to right
+//        for (int lastIndex =  intArray.length - 1; lastIndex > 0; lastIndex--) {
+//            for (int i = 0; i < lastIndex; i++) {
+//                if (intArray[i] > intArray[i+1]) {
+//                    swap(intArray, i,i+1);
+//                }
+//            }
+//        }
+//
+//        for (int i = 0; i < intArray.length; i++) {
+//            System.out.println(intArray[i]);
+//        }
 
-        for (int i = 0; i < intArray.length; i++) {
+
+
+//        int[] newArray = {20, -5, -20, 6, 9};
+//
+//        // Iterative bubblesort from right to left
+//        for( int firstIndex = 0; firstIndex < newArray.length; firstIndex++) {
+//            for (int i = newArray.length - 1; i > firstIndex; i-- ) {
+//                if (newArray[i] > newArray[i-1]) {
+//                    swap(newArray, i, i-1);
+//                }
+//            }
+//        }
+////
+        for ( int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
-        }
-
-
-
-        int[] newArray = {20, 6, 9};
-
-
-        for (int firstUnsortedIndex = 0; firstUnsortedIndex < newArray.length; firstUnsortedIndex++) {
-            for (int i = newArray.length - 1; i > firstUnsortedIndex; i-- ) {
-                if (newArray[i] > newArray[i-1]) {
-                    swap(newArray, i, i-1);
-                }
-            }
-        }
-
-        for (int i = 0; i < newArray.length; i++) {
-            System.out.println(newArray[i]);
         }
     }
 
@@ -46,5 +48,20 @@ public class bubblesort {
         array[i] = array[j];
         array[j] = aux;
     }
+
+    // Recursive bubblesort from left to right
+    public static void bubbleSort(int array[], int n) {
+        if (n == 1)
+            return;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (array[i] > array[i+1]) {
+                  swap(array, i, i+1) ;
+            }
+        }
+
+        bubbleSort(array, n - 1);
+    }
+
 }
 
