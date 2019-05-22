b66226c89155a016985e85d66ff9fa908c380331
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-22 20:22:49 +0000

    Added recursiveness for selection sort algorithm

--- selectionsort.java
+++ selectionsort.java
@@ -6,6 +6,7 @@ public class select {
 
         int[] array = {20, 57, 68, 34, -5, -10};
 
+        // Iterative selection sort from left to right
         for (int lastUnsortedIndex =  array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex-- ) {
             int largest = 0;
 
@@ -18,17 +19,18 @@ public class select {
             swap(array, largest, lastUnsortedIndex);
         }
 
-//        for ( int firstIndex = 0; firstIndex < array.length  - 1; firstIndex++) {
-//            int largest = array.length - 1;
-//
-//            for (int i = array.length - 2; i >= firstIndex; i--) {
-//                if (array[i] > array[largest]) {
-//                    largest = i;
-//                }
-//            }
-//
-//            swap (array, largest, firstIndex);
-//        }
+    // Iterative selection sort from right to left
+    //        for ( int firstIndex = 0; firstIndex < array.length  - 1; firstIndex++) {
+    //            int largest = array.length - 1;
+    //
+    //            for (int i = array.length - 2; i >= firstIndex; i--) {
+    //                if (array[i] > array[largest]) {
+    //                    largest = i;
+    //                }
+    //            }
+    //
+    //            swap (array, largest, firstIndex);
+    //        }
 
 
 
@@ -48,5 +50,43 @@ public class select {
         array[i] = array[j];
         array[j] = temp;
     }
+    
+    // Recursive selection sort from left to right
+    //    public static void selectSort(int [] array, int n) {
+    //
+    //        if (n == 2) {
+    //            return;
+    //        }
+    //
+    //        int largest = 0;
+    //        for (int i = 1; i <= n - 1; i++) {
+    //            if (array[i] > array[largest]) {
+    //                largest = i;
+    //            }
+    //        }
+    //        swap(array, largest, n-1);
+    //
+    //        selectSort(array, n-1);
+    //    }
+
+    //   Recursive selection sort from right to left
+    //   public static void selectSort(int [] array, int n) {
+
+    //     if (n == array.length - 2) {
+    //         return;
+    //     }
+
+    //     int largest = array.length - 1;
+    //     for (int i = array.length - 2; i >= n; i--) {
+    //         if (array[i] > array[largest]) {
+    //             largest = i;
+    //         }
+    //     }
+    //     if (largest != n) {
+    //         swap(array, largest, n);
+    //     }
+
+    //     selectSort(array, n+1);
+    // }
 }
 
